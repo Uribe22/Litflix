880d130529c14ade323d543659818703c4f99bc2
AndresGVx <azazelnwx@gmail.com>
2024-11-05 18:55:56 -0300

    Fusión de reseñas con cada obra

--- backend/models/Libro.js
+++ backend/models/Libro.js
@@ -13,7 +13,13 @@ const esquemaLibro = new mongoose.Schema({
   }],
   sinopsis: { type: String, required: true },
   imagen: { type: String, required: true },
-  tipo: { type: String, required: true }
+  tipo: { type: String, required: true },
+  resenia: [{
+    autor: { type: String, required: true },
+    comentario: { type: String, required: true },
+    valoracion: { type: Number, required: true },
+    fecha: { type: Date, required: true }
+  }]
 });
 
 module.exports = mongoose.model('Libro', esquemaLibro);
