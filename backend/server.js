fa7ee5b7ab605270a3c124e959e4f4b28ca615e9
AndresGVx <azazelnwx@gmail.com>
2024-11-14 02:16:58 -0300

    Funcionalidad de registro e inicio de sesión

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const pelicula = require('./models/Pelicula');
 const serie = require('./models/Serie');
 const libro = require('./models/Libro');
+const usuario = require('./models/Usuario')
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const JWT_SECRET = process.env.JWT_SECRET
 
 app.use(cors());
 app.use(express.json());
@@ -225,6 +230,75 @@ app.get('/api/buscar', async (req, res) => {
     }
 });
 
+app.post('/api/verificar-usuario', async (req, res) => {
+    const { nombre, correo } = req.body;
+  
+    try {
+      const usuarioExistente = await usuario.findOne({ $or: [{ correo }, { nombre }] });
+
+      if (!usuarioExistente) {
+        return res.status(200).json({ message: 'Disponible' });
+      }
+
+      if (usuarioExistente.correo === correo) {
+        return res.status(400).json({ message: 'Correo no disponible' });
+      }
+
+      return res.status(400).json({ message: 'Nombre de usuario no disponible' });
+
+    } catch (error) {
+      res.status(500).json({ message: 'Error al verificar usuario', error });
+    }
+});
+
+app.post('/api/usuarios', async (req, res) => {
+    const { nombre, correo, contrasenia } = req.body;
+
+    try {
+        const contraseniaEncriptada = await bcrypt.hash(contrasenia, 10);
+
+        const nuevoUsuario = new usuario( {nombre, correo, contrasenia:contraseniaEncriptada, pendientes: []} );
+        await nuevoUsuario.save();
+
+        const token = jwt.sign(
+            { usuarioId: nuevoUsuario._id, correo: nuevoUsuario.correo },
+            JWT_SECRET,
+            { expiresIn: '1h' }
+        )
+
+        res.status(201).json({ message: 'Usuario creado exitósamente', token })
+    } catch (error) {
+        res.status(400).json({ message: 'Error al crear usuario', error });
+    }
+});
+
+app.post("/api/iniciar-sesion", async (req, res) => {
+    const { correo, contrasenia } = req.body;
+  
+    try {
+      const inicio = await usuario.findOne({ correo });
+  
+      if (!inicio) {
+        return res.status(400).json({ message: "El correo no existe." });
+      }
+  
+      const esValida = await bcrypt.compare(contrasenia, inicio.contrasenia);
+  
+      if (!esValida) {
+        return res.status(400).json({ message: "Contraseña incorrecta." });
+      }
+  
+      const token = jwt.sign(
+        { userId: usuario._id },
+        JWT_SECRET,
+        { expiresIn: '1h' });
+      res.json({ token });
+  
+    } catch (error) {
+      console.error("Error en el servidor:", error);
+      res.status(500).json({ message: "Error en el servidor." });
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('¡Bienvenido a Litflix!');
